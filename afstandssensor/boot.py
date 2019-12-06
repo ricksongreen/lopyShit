2c2f2f6875ea8ca6b49c107a75f1a9dc31232b6b
ricksongreen <ricksongreen@hotmail.com>
2019-12-06 15:30:08 +0100

    afstandssensor

---
+++ afstandssensor/boot.py
@@ -0,0 +1,44 @@
+# boot.py -- run on boot-up
+
+import pycom
+
+# recommended: https://docs.pycom.io/firmwareapi/micropython/micropython/
+import micropython
+micropython.alloc_emergency_exception_buf(100)
+
+USE_WIFI = True  # connect to Wifi (True) or not (False)
+RGB_BRIGHTNESS = 0x8
+
+RGB_RED = RGB_BRIGHTNESS << 16
+RGB_GREEN = RGB_BRIGHTNESS << 8
+RGB_BLUE = RGB_BRIGHTNESS
+
+if USE_WIFI:
+    from wifimanager import WifiManager
+    try:
+        pycom.heartbeat(False)
+        wificonfig_file = 'config/wificonfig.json'
+        print('Start to connect to Wifi...({})'.format(wificonfig_file))
+        wifi = WifiManager(wificonfig_file)
+        print('Device MAC-adres: {}'.format(wifi.mac))
+
+        pycom.rgbled(RGB_RED)
+        ip = wifi.connect()  # connect device to wifi, returns IP
+        pycom.rgbled(RGB_GREEN)
+
+        print('Device is connected to Wifi: {}'.format(wifi.isconnected))
+        print('\tIP = {}'.format(ip))
+
+    except OSError as err:
+        print('OSError: file not found...{}'.format(err))
+
+    except KeyboardInterrupt:
+        print('User interrupted.')
+
+    except Exception as ex:
+        print('Exception...')
+        print(ex)
+
+    finally:
+        print('Device is connected to Wifi: {}'.format(wifi.isconnected))
+        pycom.heartbeat(True)
---
+++ afstandssensor/main.py
@@ -0,0 +1,4 @@
+from distance_sensor import DistanceSensor
+
+if __name__ == "__main__":
+    DistanceSensor()
