3561985146f2c2a143c3cf0a03856fd6665fa7e4
Cas Hooijer <cas150@hotmail.nl>
2019-12-06 15:54:47 +0100

    fix syntax error

--- server/pycom/urls.py
+++ server/pycom/urls.py
@@ -15,6 +15,6 @@ router.register(r'tag',views.TagViewSet)
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('', include(router.urls)),
 ]
